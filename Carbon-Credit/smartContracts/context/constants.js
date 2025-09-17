32d7e333aba6aa437d3b8779839c220e3e789196
Aarushi Sharma <72332111+aarushi-sharma-09@users.noreply.github.com>
2025-09-18 01:32:00 +0530

    Update constants.js

--- Carbon-Credit/smartContracts/context/constants.js
+++ Carbon-Credit/smartContracts/context/constants.js
@@ -1,3 +1,3 @@
 import SimpleCarbonCredit from './SimpleCarbonCredit.json';
-export const CC_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+export const CC_ADDRESS = '0x95B4E3691bd37e753782E9c70455445419959C7f';
 export const CC_ABI = SimpleCarbonCredit.abi;
