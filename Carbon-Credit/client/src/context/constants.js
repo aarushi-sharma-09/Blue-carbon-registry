41189599ef59527d5ed8d051f960796f14d061af
Aarushi Sharma <72332111+aarushi-sharma-09@users.noreply.github.com>
2025-09-18 00:04:44 +0530

    Update constants.js

--- Carbon-Credit/client/src/context/constants.js
+++ Carbon-Credit/client/src/context/constants.js
@@ -1,4 +1,5 @@
 import CarbonCredit from './CarbonCredit.json';
-export const CC_ADDRESS = '0x15Ef15a50a2F72126EBcDC6989044Ce6ae322802'; //this should be in the actual repo
+export const CC_ADDRESS = '0x95B4E3691bd37e753782E9c70455445419959C7f'; //this should be in the actual repo
 // export const CC_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3'; //for testing
 export const CC_ABI = CarbonCredit.abi;
+
